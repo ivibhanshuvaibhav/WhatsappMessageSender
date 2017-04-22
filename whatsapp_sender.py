dffdeb68abc3fe3b9b37ab9834015cffa5204a44
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2017-04-22 13:55:43 +0530

    Automatic whatsapp message sending script. Uses selenium to operate through whatsapp web. Takes chat name and message as input from user.

---
+++ whatsapp_sender.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+chat_name = raw_input("Enter chat-name")
+chat_message = raw_input("Enter message")
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+
+try:
+
+    driver.get("https://web.whatsapp.com")
+
+    print "Waiting for QR code to be scanned"
+
+    time.sleep(10)
+
+    search_path = "//input[@class='input input-search']"
+
+    search_box = driver.find_element_by_xpath(search_path)
+
+    search_box.send_keys(chat_name)
+
+    time.sleep(5)
+
+    print "Searching for desired chat"
+
+    path = "//div[@class='chat-title']/span[.='" + chat_name + "']"
+
+    elem = driver.find_element_by_xpath(path)
+    elem.click()
+
+    print "Chat selected"
+    print "Sending message in a while"
+
+    time.sleep(20)
+
+    input_path = "//div[@class='input']"
+    text_field = driver.find_element_by_xpath(input_path)
+
+    i = 0
+    while i < 1:
+        text_field.send_keys(chat_message)
+        text_field.send_keys(Keys.ENTER)
+        i += 1
+
+except:
+
+    print "Error occured"
+    driver.quit()
